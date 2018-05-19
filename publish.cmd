3757deb119c0f4cc6bf3f8f033aaabee2ffe67fd
Nate <fuller.nate@gmail.com>
2018-05-19 05:57:16 -0700

    init commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild AspNetCore-EchoBot-With-State.sln -p:DeployOnBuild=true -p:PublishProfile=n8botframework-Web-Deploy.pubxml -p:Password=TiyTl0fpWlZ4pzAelZddl8Jfl0v6GFcXrY55rSBdhDgWBe6rLGn34eCddh7p
+
